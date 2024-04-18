c469645c95c77a2cf3216bbcbed17183f7946539
dongmin <ldm030475@gmail.com>
2024-04-18 14:18:06 +0900

    first commit

---
+++ src/main/java/org/example/queueingserver/QueueingServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.queueingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueingServerApplication.class, args);
+    }
+
+}
