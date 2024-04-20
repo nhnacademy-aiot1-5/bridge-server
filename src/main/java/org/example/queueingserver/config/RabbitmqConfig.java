91a342a4ab9d499f5d4a17c70c1bbd8b11557c94
dongmin <ldm030475@gmail.com>
2024-04-20 22:42:33 +0900

    fix:  test중

--- src/main/java/org/example/queueingserver/config/RabbitmqConfig.java
+++ src/main/java/org/example/queueingserver/config/RabbitmqConfig.java
@@ -1,30 +1,19 @@
 package org.example.queueingserver.config;
 
-import com.rabbitmq.client.ConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:application-dev.properties")
 public class RabbitmqConfig {
-    @Value("${rabbitmq.host}")
-    private String host;
-    @Value("${rabbitmq.port}")
-    private Integer port;
-    @Value("${rabbitmq.username}")
-    private String username;
-    @Value("${rabbitmq.password}")
-    private String password;
 
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(host);
-        factory.setPort(port);
-        factory.setUsername(username);
-        factory.setPassword(password);
-        return factory;
-    }
+//    @Bean
+//    public ConnectionFactory connectionFactory() {
+//        ConnectionFactory factory = new ConnectionFactory();
+//        factory.setHost(host);
+//        factory.setPort(port);
+//        factory.setUsername(username);
+//        factory.setPassword(password);
+//        return factory;
+//    }
 }
