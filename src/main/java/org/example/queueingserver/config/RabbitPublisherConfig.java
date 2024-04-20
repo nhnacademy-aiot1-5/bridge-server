0254d89624ba2850af5a65a8a7db507c6f23ece1
dongmin <ldm030475@gmail.com>
2024-04-20 22:26:13 +0900

    fix:  관리 변경

--- src/main/java/org/example/queueingserver/config/RabbitPublisherConfig.java
+++ src/main/java/org/example/queueingserver/config/RabbitPublisherConfig.java
@@ -1,9 +1,9 @@
 package org.example.queueingserver.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
